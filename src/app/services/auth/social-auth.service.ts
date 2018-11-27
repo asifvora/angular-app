3754cd4a5c40e09898d8a3f5769751a6f35eb2a7
asif <asif.vora@multidots.com>
2018-11-27 18:54:43 +0530

    google-site-verification add

--- src/app/services/auth/social-auth.service.ts
+++ src/app/services/auth/social-auth.service.ts
@@ -3,7 +3,7 @@ import { AuthServiceConfig, GoogleLoginProvider } from "angularx-social-login";
 let config = new AuthServiceConfig([
     {
         id: GoogleLoginProvider.PROVIDER_ID,
-        provider: new GoogleLoginProvider("624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com")
+        provider: new GoogleLoginProvider("341004786579-f3655tfhkcq4ft813rjs3gh1ganmb36n.apps.googleusercontent.com")
         //624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com
 
         //341004786579-f3655tfhkcq4ft813rjs3gh1ganmb36n.apps.googleusercontent.com
--- src/app/services/books/books.service.spec.ts
+++ src/app/services/books/books.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { BooksService } from '../books.service';
+import { BooksService } from './books.service';
 
 describe('BooksService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
